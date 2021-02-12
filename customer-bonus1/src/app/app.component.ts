d77e47f719bf0dad212844170b79b44874e48f0f
anpave10 <anthonyvelizlandazuri@gmail.com>
2021-02-12 13:52:49 -0500

    add ag-grid

--- customer-bonus1/src/app/app.component.ts
+++ customer-bonus1/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'customer-bonus1';
+
+  columnDefs = [
+    { field: 'first_name', sortable:true, filter:true },
+    { field: 'last_name', sortable:true, filter:true },
+    { field: 'email', sortable:true, filter:true },
+    { field: 'phone_number' , sortable:true, filter:true}
+  ];
+
+  /*rowData=[
+    {FirstName: 'Anthony', LastName: 'Veliz', Email:'anpave_10@gmail.com', PhoneNumber: '9932812'},
+    {FirstName: 'Christian', LastName: 'Veliz', Email:'titi@gmail.com', PhoneNumber: '9962812'}
+  ];*/
+
+  rowData:any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit():void{
+    this.rowData = this.http.get('http://localhost:8000/api/customers');
+
+  }
 }
